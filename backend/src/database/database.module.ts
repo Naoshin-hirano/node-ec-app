e66b97f1977fdab5094a1f8ec2b6d2a39520a0f4
Hirano <hnaoya1120@gmail.com>
2025-05-03 01:46:45 +0900

    本番環境用のDB接続情報を追加

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -9,17 +9,29 @@ export const MIGRATION_FILES_DIR = 'dist/database/migrations/*.js';
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        database: configService.get<string>('DATABASE_NAME'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        port: Number(configService.get<string>('DATABASE_PORT')),
-        entities: [ENTITIES_DIR],
-        synchronize: false,
-        migrations: [MIGRATION_FILES_DIR],
-      }),
+      useFactory: (configService: ConfigService) => {
+        const isProduction = process.env.NODE_ENV === 'production';
+        return {
+          type: 'postgres',
+          ...(isProduction
+            ? {
+                url: configService.get<string>('DATABASE_URL'),
+                ssl: {
+                  rejectUnauthorized: false,
+                },
+              }
+            : {
+                host: configService.get<string>('DATABASE_HOST'),
+                port: Number(configService.get<string>('DATABASE_PORT')),
+                username: configService.get<string>('DATABASE_USER'),
+                password: configService.get<string>('DATABASE_PASSWORD'),
+                database: configService.get<string>('DATABASE_NAME'),
+              }),
+          entities: [ENTITIES_DIR],
+          synchronize: false,
+          migrations: [MIGRATION_FILES_DIR],
+        };
+      },
     }),
   ],
 })
